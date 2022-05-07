665c1966354d6491e74664bfd52b46026ce0d5b4
Mark Willson <cdr.nil@gmail.com>
2022-05-07 16:53:46 +0100

    Improve robustness of dict key usage
    Replace string 'constants' by Key class attributes.
    
    Define pcloud_name before use in upload_playlists.
    
    Simplify many function calls as pcloud instance holds all options
    settings.

--- playlist.py
+++ playlist.py
@@ -20,6 +20,24 @@ import urllib.parse
 import pcloudapi
 import time
 
+class Key():
+    ASPECT = 'playlist'
+    CACHE_FILE = 'cache-file'
+    CHUNK_SIZE = 'chunk-size'
+    MUSIC_FOLDER = 'music-folder'
+    DIR = 'dir'
+    PREFIX = 'prefix'
+    CREATE_CACHE = 'create-cache'
+    LIST = 'list'
+    FILEID = 'fileid'
+    CONTENTS = 'contents'
+    METADATA = 'metadata'
+    NAME = 'name'
+    COLLECTION = 'collection'
+    COLLECTIONS = 'collections'
+    ID = 'id'
+    PATH = 'path'
+
 def walk(folder, root, fileid):
     '''Recursively walk the pCloud directory structure.
 
@@ -29,11 +47,11 @@ def walk(folder, root, fileid):
 
     '''
     for entry in folder:
-        name = entry['name']
+        name = entry[Key.NAME]
         if name.endswith('.mp3'):
-            fileid[f'{root}/{name}'] = entry['fileid']
-        if 'contents' in entry:
-            walk(entry['contents'], f'{root}/{name}', fileid)
+            fileid[f'{root}/{name}'] = entry[Key.FILEID]
+        if Key.CONTENTS in entry:
+            walk(entry[Key.CONTENTS], f'{root}/{name}', fileid)
     return
 
 def mp3_dict(root_folder):
@@ -54,16 +72,17 @@ def read_m3u_file(filename, remove='/rep/music'):
         lines = f.readlines()
     return [line.replace(remove, '').strip()  for line in lines]
 
-def create_playlist(pcloud, name, ids, chunk_size=100):
+def create_playlist(pcloud, name, ids):
     '''Create pCloud playlist.
 
     name contains the playlist name, while tracks are identified by
     the list ids. chunk-size controls how many fileids are uploaded in
     each call to the pCloud API.
     '''
+    chunk_size = pcloud.config[Key.ASPECT][Key.CHUNK_SIZE]
     chunk_ids, next_ids = pcloudapi.chunked(ids, chunk_size)
     result = pcloud.collection_create(name, chunk_ids)
-    coll_id = result['collection']['id']
+    coll_id = result[Key.COLLECTION][Key.ID]
     nchunks = 1
     while next_ids:
         chunk_ids, next_ids = pcloudapi.chunked(next_ids, chunk_size)
@@ -79,13 +98,16 @@ def pcloud_playlist_names(pcloud):
     '''
     pcloud_dict = dict()
     pcloud_playlist = pcloud.collection_list()
-    for coll in pcloud_playlist['collections']:
-        pcloud_dict[coll['name']] = coll['id']
+    for coll in pcloud_playlist[Key.COLLECTIONS]:
+        pcloud_dict[coll[Key.NAME]] = coll[Key.ID]
     return pcloud_dict
 
-def upload_playlists(pcloud, fileids, files, dir, m3u_prefix,
-                     chunk_size, verbose):
+def upload_playlists(pcloud, fileids, files):
     '''Convert and upload local m3u playlists to pCloud playlists.'''
+    chunk_size = pcloud.config[Key.ASPECT][Key.CHUNK_SIZE]
+    m3u_prefix = pcloud.config[Key.ASPECT][Key.PREFIX]
+    dir = pcloud.config[Key.ASPECT][Key.DIR]
+    verbose = pcloud.config[pcloudapi.Key.VERBOSE]
     # get existing pCloud playlists dict (name => fileid)
     pcloud_playlists = pcloud_playlist_names(pcloud)
     for file in files:
@@ -94,11 +116,11 @@ def upload_playlists(pcloud, fileids, files, dir, m3u_prefix,
         if not os.path.exists(file):
             pcloudapi.error(f'playlist file does not exist: {file}',die=False)
             continue
+        pcloud_name = os.path.basename(file).replace('.m3u', '')
         if verbose:
             print(f'Creating playlist {pcloud_name} ... ', end='')
             sys.stdout.flush()
         m3u = read_m3u_file(file,remove=m3u_prefix)
-        pcloud_name = os.path.basename(file).replace('.m3u', '')
         if pcloud_name in pcloud_playlists:
             pcloud.collection_delete(pcloud_playlists[pcloud_name])
             time.sleep(1)
@@ -107,31 +129,22 @@ def upload_playlists(pcloud, fileids, files, dir, m3u_prefix,
         except KeyError as err:
             pcloudapi.error(f'playlist track not found on pCloud '
                             f'(stale cache?): {err}')
-        nchunks = create_playlist(pcloud, urllib.parse.quote(pcloud_name), ids,
-                                  chunk_size=chunk_size)
+        nchunks = create_playlist(pcloud, urllib.parse.quote(pcloud_name), ids)
         if verbose: print(f'done using {nchunks} chunks.')
     return
 
-def validate_config(cmd_config, config):
-    playlist = cmd_config['playlist']
+def validate_config(config):
+    playlist = config[Key.ASPECT]
 
-    # if music-folder provided on command line, any existing cache must
-    # be re-created.
-    if playlist['music-folder'] != config['playlist']['music-folder'] and \
-       playlist['cache-file']:
-        playlist['create_cache'] = True
-
-    # validate config
-    if 'create_cache' in playlist and not \
-       playlist['cache-file']:
+    if Key.CREATE_CACHE in playlist and not playlist[Key.CACHE_FILE]:
         pcloudapi.error('cache file name must be provided for create')
 
-    chunk_size = int(playlist['chunk-size'])
+    chunk_size = int(playlist[Key.CHUNK_SIZE])
     # more than 300 fileids seems to cause "Remote end closed
     # connection without response"
     if chunk_size <= 0 or chunk_size > 300:
         pcloudapi.error(f'invalid chunk size specified: {chunk_size}')
-    playlist['chunk-size'] = chunk_size
+    playlist[Key.CHUNK_SIZE] = chunk_size
 
     return
 
@@ -141,14 +154,16 @@ def list_playlists(pcloud):
         print(name)
     return
 
-def process_playlists(pcloud, pl_config, verbose, pl_files):
+def process_playlists(pcloud, pl_files):
+    '''Create pCloud playlist file (collection) for each .m3u file in the
+       list pl_files'''
+
+    pl_config = pcloud.config[Key.ASPECT]
     cache_file =  os.path.expanduser(
-        os.path.expandvars(pl_config['cache-file']))
-    create_cache = 'create-cache' in pl_config
-    chunk_size = pl_config['chunk-size']
-    music_folder = pl_config['music-folder']
-    dir = pl_config['dir']
-    prefix = pl_config['prefix']
+        os.path.expandvars(pl_config[Key.CACHE_FILE]))
+    create_cache = Key.CREATE_CACHE in pl_config
+    music_folder = pl_config[Key.MUSIC_FOLDER]
+    verbose = pcloud.config[pcloudapi.Key.VERBOSE]
 
     if (cache_file and not os.path.exists(cache_file)) or \
        create_cache or not cache_file:
@@ -162,40 +177,41 @@ def process_playlists(pcloud, pl_config, verbose, pl_files):
     else:
         if verbose: print('Loading music collection from cache file ...')
         folder = pcloudapi.load_json(cache_file)
-
+        if pl_config[Key.MUSIC_FOLDER] != folder[Key.METADATA][Key.PATH]:
+            if verbose:
+                print('Cache file doesn\'t match music-folder: loading '\
+                      'music collection from pCloud ...')
+            folder = pcloud.list_folder(path=music_folder)
     try:
-        fileids = mp3_dict(folder['metadata']['contents'])
+        fileids = mp3_dict(folder[Key.METADATA][Key.CONTENTS])
     except KeyError as err:
         pcloudapi.error(f'unable to decode music_folder; corrupt cache?')
 
-    upload_playlists(pcloud, fileids, pl_files, dir, prefix, chunk_size,
-                     verbose)
+    upload_playlists(pcloud, fileids, pl_files)
     return
 
 def main():
     # default playlist options
     playlist = {
-        'cache-file': '',
-        'chunk-size': 100,
-        'music-folder': '/Music',
-        'dir': '',
-        'prefix': ''}
-    aspect_key = 'playlist'
-    aspect_opts = [opt+'=' for opt in playlist.keys()] + \
-        ['create-cache','list']
+        Key.CACHE_FILE: '',
+        Key.CHUNK_SIZE: 100,
+        Key.MUSIC_FOLDER: '/Music',
+        Key.DIR: '',
+        Key.PREFIX: ''}
 
-    pcloud = pcloudapi.PCloud(aspect_key, playlist)
-    config, args = pcloudapi.merge_command_options(pcloud.config, aspect_key,
-                                                   aspect_opts)
-    validate_config(config, pcloud.config)
+    aspect_opts = [opt+'=' for opt in playlist.keys()] + \
+        [Key.CREATE_CACHE,Key.LIST]
+
+    pcloud = pcloudapi.PCloud(Key.ASPECT, playlist)
+    args = pcloud.merge_command_options(Key.ASPECT, aspect_opts)
+    validate_config(pcloud.config)
 
     try:
-        pcloud.authenticate('reauth' in config)
-        if 'list' in config[aspect_key]:
+        pcloud.authenticate()
+        if Key.LIST in pcloud.config[Key.ASPECT]:
             list_playlists(pcloud)
         else:
-            process_playlists(pcloud, config[aspect_key], config['verbose'],
-                              args)
+            process_playlists(pcloud, args)
     except pcloudapi.PCloudException as err:
         pcloudapi.error(f'error: {err.code}; message: {err.msg}\n'\
                         f'Url: {err.url}')
