4c18a148316d3bc8283966b3bf276b49b3fdba72
Mark Willson <cdr.nil@gmail.com>
2026-06-15 09:59:24 +0100

    Additional tests

--- test.sh
+++ test.sh
@@ -5,22 +5,22 @@
 set -e
 
 create_test_dirs() {
-    rm -rf test test.orig
-    mkdir -p test/d1
-    mkdir test/d2
-    mkdir test/d3
-    mkdir test/d2/d4
-    echo 'x contents' >test/x
-    echo 'y contents' >test/y
-    echo 'z contents' >test/z
-    cp test/x test/y test/z test/d2
-    cp test/x test/y test/z test/d3
-    cp test/x test/y test/z test/d2/d4
+    rm -rf $1/test $1/test.orig
+    mkdir -p $1/test/d1
+    mkdir $1/test/d2
+    mkdir $1/test/d3
+    mkdir $1/test/d2/d4
+    echo 'x contents' >$1/test/x
+    echo 'y contents' >$1/test/y
+    echo 'z contents' >$1/test/z
+    cp $1/test/x $1/test/y $1/test/z $1/test/d2
+    cp $1/test/x $1/test/y $1/test/z $1/test/d3
+    cp $1/test/x $1/test/y $1/test/z $1/test/d2/d4
 }
 
 echo "Running smoke test of pcutil.py. Expect no errors."
-
-create_test_dirs
+TMP=$(mktemp -d)
+create_test_dirs ${TMP}
 
 python pcutil.py cp -d pcutil.py p:/new_pcutil.py
 python pcutil.py cp pcutil.py p:/new_pcutil.py
@@ -28,31 +28,39 @@ python pcutil.py cp pcutil.py p:/new_pcutil.py
 python pcutil.py cp -d pcutil.py p:/
 python pcutil.py cp pcutil.py p:/
 
-python pcutil.py cp -d p:/new_pcutil.py test/new1_pcutil.py
-python pcutil.py cp p:/new_pcutil.py test/new1_pcutil.py
+python pcutil.py cp -d p:/new_pcutil.py ${TMP}/test/new1_pcutil.py
+python pcutil.py cp p:/new_pcutil.py ${TMP}/new1_pcutil.py
+ls -l ${TMP}/new1_pcutil.py
 
-python pcutil.py cp -d p:/new_pcutil.py new_dir/new1_pcutil.py
-python pcutil.py cp p:/new_pcutil.py new_dir/new1_pcutil.py
-ls -l new_dir
-rm -r new_dir
+python pcutil.py cp pcutil.py p:/nd1/nd2/pcutil.py
+python pcutil.py cp p:/nd1/nd2/pcutil.py ${TMP}
+ls -l ${TMP}/pcutil.py
 
-python pcutil.py cp -dr test p:/
-python pcutil.py cp -r test p:/
-mv test test.orig
+python pcutil.py cp -d p:/new_pcutil.py ${TMP}/new1_pcutil.py
+python pcutil.py cp p:/new_pcutil.py ${TMP}/new1_pcutil.py
+ls -l ${TMP}/new1_pcutil.py
 
-python pcutil.py cp -dr p:/test .
-python pcutil.py cp -r p:/test .
-diff -r test test.orig
+python pcutil.py cp -dr ${TMP}/test p:/
+python pcutil.py cp -r ${TMP}/test p:/
+mv ${TMP}/test ${TMP}/test.orig
 
-python pcutil.py cp -dr p:/test/ x/y/test
-python pcutil.py cp -r  p:/test/ x/y/test
-diff -r test x/y/test
+python pcutil.py cp -dr p:/test ${TMP}
+python pcutil.py cp -r p:/test ${TMP}
+diff -r ${TMP}/test ${TMP}/test.orig
 
-python pcutil.py cp pcutil.py p:/new_dir/x
+python pcutil.py cp -dr p:/test/ ${TMP}/x/y/test
+python pcutil.py cp -r  p:/test/ ${TMP}/x/y/test
+diff -r ${TMP}/test ${TMP}/x/y/test
 
-rm -rf test test.orig x
+python pcutil.py cp pcutil.py p:/new_dir/new_file_name
+python pcutil.py cp p:/new_dir/new_file_name ${TMP}
+ls -l ${TMP}/new_file_name
 
-echo Removing test folders and files from pCloud
-python pcutil.py rm /pcutil.py /new_pcutil.py new_dir/x
+rm -rf ${TMP}
+
+echo "Removing test folders and files from pCloud in five seconds"
+sleep 5
+python pcutil.py rm /pcutil.py /new_pcutil.py new_dir/new_file_name
+sleep 1
 python pcutil.py rm /new_dir
-python pcutil.py rm -r /test
+python pcutil.py rm -r /test /nd1
