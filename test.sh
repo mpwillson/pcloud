47baa70dbed0ab6ea18fb4fb302edb9256d38df7
Mark Willson <cdr.nil@gmail.com>
2026-06-16 07:47:11 +0100

    Use unlikely-to-exist folder name on pCloud

--- test.sh
+++ test.sh
@@ -20,47 +20,50 @@ create_test_dirs() {
 
 echo "Running smoke test of pcutil.py. Expect no errors."
 TMP=$(mktemp -d)
+PTMP=$(echo $TMP|sed -e 's!/tmp/tmp.!!')
+
 create_test_dirs ${TMP}
 
-python pcutil.py cp -d pcutil.py p:/new_pcutil.py
-python pcutil.py cp pcutil.py p:/new_pcutil.py
+python pcutil.py cp -d pcutil.py p:/${PTMP}/new_pcutil.py
+python pcutil.py cp pcutil.py p:/${PTMP}/new_pcutil.py
 
-python pcutil.py cp -d pcutil.py p:/
-python pcutil.py cp pcutil.py p:/
+python pcutil.py cp -d pcutil.py p:/${PTMP}
+python pcutil.py cp pcutil.py p:/${PTMP}
 
-python pcutil.py cp -d p:/new_pcutil.py ${TMP}/test/new1_pcutil.py
-python pcutil.py cp p:/new_pcutil.py ${TMP}/new1_pcutil.py
+python pcutil.py cp -d p:/${PTMP}/new_pcutil.py ${TMP}/test/new1_pcutil.py
+python pcutil.py cp p:/${PTMP}/new_pcutil.py ${TMP}/new1_pcutil.py
 ls -l ${TMP}/new1_pcutil.py
 
-python pcutil.py cp pcutil.py p:/nd1/nd2/pcutil.py
-python pcutil.py cp p:/nd1/nd2/pcutil.py ${TMP}
+python pcutil.py cp pcutil.py p:/${PTMP}/nd1/nd2/pcutil.py
+python pcutil.py cp p:/${PTMP}/nd1/nd2/pcutil.py ${TMP}
 ls -l ${TMP}/pcutil.py
 
-python pcutil.py cp -d p:/new_pcutil.py ${TMP}/new1_pcutil.py
-python pcutil.py cp p:/new_pcutil.py ${TMP}/new1_pcutil.py
+python pcutil.py cp -d p:/${PTMP}/new_pcutil.py ${TMP}/new1_pcutil.py
+python pcutil.py cp p:/${PTMP}/new_pcutil.py ${TMP}/new1_pcutil.py
 ls -l ${TMP}/new1_pcutil.py
 
-python pcutil.py cp -dr ${TMP}/test p:/
-python pcutil.py cp -r ${TMP}/test p:/
+python pcutil.py cp -dr ${TMP}/test p:/${PTMP}
+python pcutil.py cp -r ${TMP}/test p:/${PTMP}
 mv ${TMP}/test ${TMP}/test.orig
 
-python pcutil.py cp -dr p:/test ${TMP}
-python pcutil.py cp -r p:/test ${TMP}
+python pcutil.py cp -dr p:/${PTMP}/test ${TMP}
+python pcutil.py cp -r p:/${PTMP}/test ${TMP}
 diff -r ${TMP}/test ${TMP}/test.orig
 
-python pcutil.py cp -dr p:/test/ ${TMP}/x/y/test
-python pcutil.py cp -r  p:/test/ ${TMP}/x/y/test
+python pcutil.py cp -dr p:/${PTMP}/test/ ${TMP}/x/y/test
+python pcutil.py cp -r  p:/${PTMP}/test/ ${TMP}/x/y/test
 diff -r ${TMP}/test ${TMP}/x/y/test
 
-python pcutil.py cp pcutil.py p:/new_dir/new_file_name
-python pcutil.py cp p:/new_dir/new_file_name ${TMP}
+python pcutil.py cp pcutil.py p:/${PTMP}/new_dir/new_file_name
+python pcutil.py cp p:/${PTMP}/new_dir/new_file_name ${TMP}
 ls -l ${TMP}/new_file_name
 
 rm -rf ${TMP}
 
 echo "Removing test folders and files from pCloud in five seconds"
 sleep 5
-python pcutil.py rm /pcutil.py /new_pcutil.py new_dir/new_file_name
+python pcutil.py rm ${PTMP}/pcutil.py ${PTMP}/new_pcutil.py ${PTMP}/new_dir/new_file_name
 sleep 1
-python pcutil.py rm /new_dir
-python pcutil.py rm -r /test /nd1
+python pcutil.py rm ${PTMP}/new_dir
+python pcutil.py rm -r ${PTMP}/test ${PTMP}/nd1
+python pcutil.py rm ${PTMP}
